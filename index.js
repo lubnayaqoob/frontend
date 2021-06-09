e92884ed458a292cce6b9216724f094aa2eeb139
DESKTOP-U15TEL8\Lubna Yaqoob <lubna.yaqoob@alphin.com>
2021-06-09 14:30:08 +0200

    updated code

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 const port = 8080;
+var express = require("express"),
+app = express();
+var bodyParser = require("body-parser");
 
-var express = require('express'),
-    app = express();
-var bodyParser = require('body-parser');
+app.use(express.static(__dirname + "/public"));
 
-app.use(express.static(__dirname + '/public'));
-
-app.use(bodyParser.urlencoded({
-   extended: false
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
 app.use(bodyParser.json());
 
-app.get('/', function(req, res){
-  res.render('form');// if jade
-  // You should use one of line depending on type of frontend you are with
-  res.sendFile(__dirname + '/form.html'); //if html file is root directory
- res.sendFile("index.html"); //if html file is within public directory
+app.get("/", function (req, res) {
+  res.render("form"); // if jade
+  res.sendFile(__dirname + "/form.html"); //if html file is root directory
+  res.sendFile("index.html"); //if html file is within public directory
 });
 
-app.post('/',function(req,res){
-  
-   var ferightWeight = req.body.weight;
-   var pickUpLocation = req.body.pickUpLocation;
-   var dropOffLocation = req.body.dropOffLocation;
-   var searchParams = {
-     'weight' : ferightWeight,
-     'pickUpLocation' : pickUpLocation,
-     'dropOffLocation' :dropOffLocation
-    }
+app.post("/", function (req, res) {
+  var ferightWeight = req.body.weight;
+  var pickUpLocation = req.body.pickUpLocation;
+  var dropOffLocation = req.body.dropOffLocation;
+  var searchParams = {
+    weight: ferightWeight,
+    pickUpLocation: pickUpLocation,
+    dropOffLocation: dropOffLocation,
+  };
 
-   res.send(searchParams);
-   console.log(searchParams);
+  res.send(searchParams);
+  console.log(searchParams);
 });
 
 app.listen(port);
+console.log(`Example app listening at http://localhost:${port}`);
